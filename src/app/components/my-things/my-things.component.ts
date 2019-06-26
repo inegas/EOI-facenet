8b0fe37bfa6b805429f48629df8aa03cbd2be7d4
Angel Banegas <mcinegas@gmail.com>
2019-06-26 20:16:53 +0200

    Codigo refactorizado

--- src/app/components/my-things/my-things.component.ts
+++ src/app/components/my-things/my-things.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Historie } from 'src/app/entities/historie-model';
-import { User } from 'src/app/entities/user-model';
-import { UserService } from 'src/app/services/user.service';
-import { HistoriesService } from 'src/app/services/histories.service';
 
 @Component({
   selector: 'app-my-things',
@@ -11,49 +7,10 @@ import { HistoriesService } from 'src/app/services/histories.service';
 })
 export class MyThingsComponent implements OnInit {
 
- user:User
-  historie:Historie;
-  allHistories:Historie[];
-  
+ngOnInit(){
 
-  constructor(
-    private userService:UserService,
-    private historieService:HistoriesService) { }
+}
 
-  ngOnInit() {
-    this.getUser();
-    this.getHistories();
-    this.historie = new Historie();
-    this.historie.id = null;
-    this.historie.user =  new User();
-    this.historie.date = Date.now();
-  }
-
-  getUser(){
-    this.userService.getUser().subscribe(
-      (data:User) => {
-        this.user = data;
-      },
-      error => console.log(error),
-      () => console.log('user load')
-    );
-  };
-
-  getHistories(){
-    this.historieService.getHistories().subscribe(
-      (data:Historie[]) =>{
-        this.allHistories = data;
-      }
-    );
-  };
-
-  postHistorie(historie:Historie){
-    this.historie.user.id = this.user.id;
-    this.historie.user.username = this.user.username;
-    this.historie.user.img = this.user.img;
-    this.historieService.postHistorie(historie).subscribe(
-      () => this.getHistories()
-    );
-  };
+constructor(){}
 
 }
--- src/app/components/profile-view/profile-view.component.ts
+++ src/app/components/profile-view/profile-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/entities/user-model';
+import { Historie } from 'src/app/entities/historie-model';
+import { UserService } from 'src/app/services/user.service';
+import { HistoriesService } from 'src/app/services/histories.service';
 
 @Component({
   selector: 'app-profile-view',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileViewComponent implements OnInit {
 
-  constructor() { }
+   user:User
+  historie:Historie;
+  allHistories:Historie[];
+  
+
+  constructor(
+    private userService:UserService,
+    private historieService:HistoriesService) { }
 
   ngOnInit() {
+    this.getUser();
+    this.getHistories();
+    this.historie = new Historie();
+    this.historie.id = null;
+    this.historie.user =  new User();
+    this.historie.date = Date.now();
   }
 
+  getUser(){
+    this.userService.getUser().subscribe(
+      (data:User) => {
+        this.user = data;
+      },
+      error => console.log(error),
+      () => console.log('user load')
+    );
+  };
+
+  getHistories(){
+    this.historieService.getHistories().subscribe(
+      (data:Historie[]) =>{
+        this.allHistories = data;
+      }
+    );
+  };
+
+  postHistorie(historie:Historie){
+    this.historie.user.id = this.user.id;
+    this.historie.user.username = this.user.username;
+    this.historie.user.img = this.user.img;
+    this.historieService.postHistorie(historie).subscribe(
+      () => this.getHistories()
+    );
+  };
 }
